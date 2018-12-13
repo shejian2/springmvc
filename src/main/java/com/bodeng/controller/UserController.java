8e7552bf0d9e5a7c80d45cf51f3f81875e8ca82a
dengbo <3083760015@qq.com>
2018-12-13 19:19:01 +0800

    添加addUser

--- src/main/java/com/bodeng/controller/UserController.java
+++ src/main/java/com/bodeng/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -16,6 +17,23 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+
+    @RequestMapping("/addUser")
+    @ResponseBody
+    public boolean addUser(@RequestParam(value = "username",required = false) String username,
+                           @RequestParam(value = "age",required = false) int age){
+        if(username!=null&&!username.equals("")){
+            UserModel userModel=new UserModel();
+            userModel.setUsername(username);
+            userModel.setAge(age);
+            userService.addUser(userModel);
+            return true;
+        }
+
+
+        return false;
+    }
 
 
     @RequestMapping("/getAllUsers")
